304573e3aa9cfb2b08a13982ea151298d8a3ad24
Kedersen <62950601+KedBlaiz@users.noreply.github.com>
2020-06-22 11:07:14 -0400

    index.html

---
+++ main.js
@@ -0,0 +1,15 @@
+        
+
+const button = document.createElement('button')
+button.innerText = 'REMOVE'
+const bodyElement = document.getElementsByTagName('body')[0]
+bodyElement.append(button)
+
+
+
+
+const mainElement = document.querySelector('main')
+const img = document.createElement('img')
+
+img.src = 'https://www.designnews.com/sites/default/files/Design%20News/P90301922_highRes_bmw-i8-roadster-04-2.jpg' 
+mainElement.appendChild(img).style.height="500px"
